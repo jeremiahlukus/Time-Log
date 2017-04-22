7868ee00cb42d56b858b7750133524298eb746bc
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2017-04-22 00:27:42 -0400

    Setup zeus, created index action for posts

--- Gemfile
+++ Gemfile
@@ -17,7 +17,6 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'devise'
 
-
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,8 +2,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'spec_helper'
+
 require 'rspec/rails'
+require 'spec_helper'
 require 'capybara/rails'
 
 ActiveRecord::Migration.maintain_test_schema!
