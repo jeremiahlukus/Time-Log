df8ce8e07430c5186953370fcea87a96993d4065
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2017-06-12 11:37:08 -0400

    Added status to admin post field, trying to search using enum

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
- @user = User.create(email: "test@test.com", password: "heyhey", password_confirmation: "heyhey", first_name: "John",     last_name: "Doe")
+ @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John",     last_name: "Doe")
 
  puts "One user has been created"
+
+
+ AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin",     last_name: "User")
 
 100.times do |post| 
   Post.create!(date: Date.today, rationale: "#{post} rationale contect", user_id: @user.id)
