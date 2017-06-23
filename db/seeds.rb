5557c75957854c0c666cc722f72262f4a58583ee
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2017-06-23 10:40:35 -0400

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,6 @@ end
 puts "100 posts have have been created"
 
 100.times do |audit_log| 
-  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end
 puts "100 audit_logs have have been created"
