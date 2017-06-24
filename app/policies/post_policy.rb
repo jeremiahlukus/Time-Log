87217d05c8ed99f0eaada0f3e6f5875074becd68
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2017-06-24 16:48:23 -0400

    Added new view elements for items that are pending

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,7 +1,9 @@
 class PostPolicy < ApplicationPolicy
+
   def approve?
     admin?
   end
+
   def update?
     return true if post_approved? && admin?
     return true if user_or_admin && !post_approved?
